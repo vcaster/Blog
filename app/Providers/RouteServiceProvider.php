a5f19c4c337b09d5fe87165166dcd36fc00621e2
ainaniran <ainaniran@yahoo.com>
2018-08-31 06:56:06 +0100

    seo friendly *slug*

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use App\Post;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -23,9 +24,11 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
+        Route::bind('post', function ($slug)
+        {
+          return Post::where('slug', $slug)->first();
+        });
     }
 
     /**
