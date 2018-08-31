13dbdbfa46a5688b75db1975f23b036d29146884
ainaniran <ainaniran@yahoo.com>
2018-08-31 19:44:25 +0100

    markdown done

--- config/app.php
+++ config/app.php
@@ -124,6 +124,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -177,6 +178,7 @@ return [
 
     'aliases' => [
 
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
